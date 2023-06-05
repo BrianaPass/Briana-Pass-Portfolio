4d3f42cb7531d34588f1bd50bb7b61da24aa598e
BrianaPass <116980413+BrianaPass@users.noreply.github.com>
2023-06-05 13:46:03 -0400

    update about page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Briana</span>
+            Hi, I'm <span className='text-[#e3026f]'>Briana</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a software engineer. <br className='sm:block hidden' />
@@ -25,7 +25,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      <ComputersCanvas/>
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
