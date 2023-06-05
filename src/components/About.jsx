571a467734d918809e6e877c14f90b6af881ddb2
BrianaPass <116980413+BrianaPass@users.noreply.github.com>
2023-06-05 14:03:30 -0400

    fixed bug

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles.js";
 import { services } from "../constants";
-import { SectionWrapper } from "../src/hoc";
+import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion.js";
 
 const ServiceCard = ({ index, title, icon }) => (
