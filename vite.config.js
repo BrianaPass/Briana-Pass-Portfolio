c9ccba64a49b18b0e627f95463d8d189a0d60ad6
BrianaPass <116980413+BrianaPass@users.noreply.github.com>
2023-07-10 16:34:27 -0400

    Revert "added yml"
    This reverts commit 0a8982b68672d33c8eab8fdd9f00db562cb9e815.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://github.com/BrianaPass/Briana-Pass-Portfolio',
   plugins: [react()],
 })
